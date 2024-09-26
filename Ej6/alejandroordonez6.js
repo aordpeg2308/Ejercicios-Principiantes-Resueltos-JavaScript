866a41e2289f413ea3d602bca8c35bebc5ffcc7b
Alejandro Ordonez <alejandroordonezp@gmail.com>
2024-09-26 08:54:46 +0200

    Añadidos Ej6 y Ej7

---
+++ Ej6/alejandroordonez6.js
@@ -0,0 +1,12 @@
+// El primero devuelve true debido a que pide que se cumpla una de las dos condiciiones y la segunda lo hace
+let x = 30 ;
+let y = "a"; 
+y === "b" || x >= 30
+
+
+
+
+// El segundo  devolverá False debido a que  la primera condicion si se cumple mientras que la segunda es false.let x = 15 
+x = 15;
+y = 2 ;
+!( x == "15" || x === y ) && ! (y !== 2 && x <= y);
---
+++ Ej7/alejandroordonez7.js
@@ -0,0 +1,8 @@
+let nombre = "pepe";
+let nombre2 = "jose";
+let numero = "1";
+let numero2 = 1;
+console.log(nombre.length == nombre2.length);
+console.log(nombre == nombre2);
+console.log(numero === numero2);
+console.log(numero == numero2);
